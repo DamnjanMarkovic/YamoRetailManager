3c83b4c8df7415b6bf72aa508646c804f8e872bb
Damnjan Markovic <damnjan.markovic@hotmail.com>
2021-08-21 20:17:17 +0200

    YamoDataManager added.

---
+++ YamoDataManager/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace YamoDataManager.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ YamoDataManager/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(YamoDataManager.Startup))]
+
+namespace YamoDataManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
